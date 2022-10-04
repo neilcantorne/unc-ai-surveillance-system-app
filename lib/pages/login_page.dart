83c63cb8f2a5e5b224aa962234af8ba606a40986
neilcantorne <cantorne.neil@gmail.com>
2022-10-04 17:50:47 +0800

    add init login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool rememberPassword = false;
+
+  @override
+  Widget build(BuildContext context) => Form(
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person), hintText: "Username"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.key), hintText: "Password"),
+              obscureText: true,
+            ),
+          ),
+          Row(children: [
+            Checkbox(
+                value: rememberPassword,
+                onChanged: (value) => {
+                      if (value != null)
+                        {setState(() => rememberPassword = value)}
+                    }),
+            const Text("Remember me")
+          ]),
+          const Spacer(),
+          SizedBox(
+            height: 40,
+            child:
+                ElevatedButton(onPressed: () => {}, child: const Text("Login")),
+          )
+        ]),
+      );
+}
