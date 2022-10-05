bc626caf2c20f37c05531c318446e2d73bfd3541
neilcantorne <cantorne.neil@gmail.com>
2022-10-06 01:54:12 +0800

    impl json serialization

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -9,4 +9,17 @@ class User {
       required this.username,
       required this.firstName,
       required this.lastName});
+
+  User.fromJson(Map<String, dynamic> json)
+      : id = json["id"],
+        username = json["username"],
+        firstName = json["first-name"],
+        lastName = json["last-name"];
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "username": username,
+        "first-name": firstName,
+        "last-name": lastName,
+      };
 }
