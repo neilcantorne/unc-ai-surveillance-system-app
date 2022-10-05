7c13dcffc987b7d49d3db9aba9756765e1949996
neilcantorne <cantorne.neil@gmail.com>
2022-10-06 01:49:49 +0800

    add user model

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+class User {
+  String id;
+  String username;
+  String firstName;
+  String lastName;
+
+  User(
+      {required this.id,
+      required this.username,
+      required this.firstName,
+      required this.lastName});
+}
