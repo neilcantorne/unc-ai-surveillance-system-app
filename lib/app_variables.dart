fee0656bc732810b2d02cf87e11e211733c61aeb
neilcantorne <cantorne.neil@gmail.com>
2022-10-20 02:35:16 +0800

    update url

--- lib/app_variables.dart
+++ lib/app_variables.dart
@@ -9,7 +9,7 @@ class AppVariables {
 
   static Future<AppVariables> get variables async {
     final options = BaseOptions(
-        baseUrl: 'http://127.0.0.1:8080',
+        baseUrl: 'http://localhost:8080',
         connectTimeout: 5000,
         receiveTimeout: 3000,
         contentType: "application/json");
