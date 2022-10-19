2e92f7e413a595757c37386472509755dd9d8d85
neilcantorne <cantorne.neil@gmail.com>
2022-10-19 17:36:25 +0800

    improve styles and pass app vars

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:unc_ai_surveillance_system_app/pages/login_page.dart';
+import 'package:unc_ai_surveillance_system_app/app_vars.dart';
+import 'package:unc_ai_surveillance_system_app/screens/start_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,48 +15,55 @@ class MyApp extends StatelessWidget {
     const primaryColor = Color.fromRGBO(225, 42, 32, 1.0);
     const secondaryColor = Color.fromRGBO(225, 225, 225, 1.0);
     const defaultTextColor = Color.fromRGBO(125, 125, 125, 1.0);
-        
+    final buttonStyle = ButtonStyle(
+        backgroundColor:
+            MaterialStateColor.resolveWith((states) => primaryColor),
+        foregroundColor:
+            MaterialStateColor.resolveWith((states) => Colors.white),
+        textStyle: MaterialStateTextStyle.resolveWith((states) => TextStyle(
+              fontSize: Theme.of(context).textTheme.button?.fontSize,
+              fontWeight: FontWeight.w500,
+            )),
+        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+            side: const BorderSide(color: Colors.red))),
+        padding: MaterialStateProperty.resolveWith(
+            (states) => const EdgeInsets.symmetric(vertical: 16)));
+
+    final appVars = AppVars();
+
     return MaterialApp(
       title: 'UNC: COVID-19 Protocols AI Surveillance System',
-      theme: ThemeData(
-          primaryColor: primaryColor,
-          backgroundColor: Colors.white,
-          inputDecorationTheme: InputDecorationTheme(
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide:  const BorderSide(
-                      width: 0,
-                      style: BorderStyle.none
-                  )
-              ),
-              filled: true,
-              fillColor: secondaryColor,
-              contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 1),
-              hintStyle: TextStyle(
-                  fontSize: Theme.of(context).textTheme.button?.fontSize,
-                  fontWeight: FontWeight.w500,
-                  color: defaultTextColor
-              )
-          ),
-        textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-              backgroundColor: MaterialStateColor.resolveWith((states) => primaryColor),
-              foregroundColor: MaterialStateColor.resolveWith((states) => Colors.white),
-              textStyle: MaterialStateTextStyle.resolveWith((states) => TextStyle(
-                  fontSize: Theme.of(context).textTheme.button?.fontSize,
-                  fontWeight: FontWeight.w500,
-              )),
-              shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(100),
-                      side: const BorderSide(color: Colors.red)
-                    )
-              ),
-              padding: MaterialStateProperty.resolveWith((states) => const EdgeInsets.symmetric(vertical: 16))
-          )
-        ),
+      theme: ThemeData.light().copyWith(
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+        primaryColor: primaryColor,
+        checkboxTheme: CheckboxThemeData(
+            fillColor: MaterialStateColor.resolveWith((states) {
+          if (states.contains(MaterialState.selected)) {
+            return primaryColor;
+          }
+          return defaultTextColor;
+        })),
+        backgroundColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100),
+                borderSide:
+                    const BorderSide(width: 0, style: BorderStyle.none)),
+            filled: true,
+            fillColor: secondaryColor,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 1),
+            hintStyle: TextStyle(
+                fontSize: Theme.of(context).textTheme.button?.fontSize,
+                fontWeight: FontWeight.w500,
+                color: defaultTextColor)),
+        elevatedButtonTheme: ElevatedButtonThemeData(style: buttonStyle),
+        textButtonTheme: TextButtonThemeData(style: buttonStyle),
       ),
-      home: const LoginPage(),
+      home: StartScreen(
+        vars: appVars,
+      ),
     );
   }
 }
