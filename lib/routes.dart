6485c651beacd090e83e23089290f00c5082bd08
neilcantorne <cantorne.neil@gmail.com>
2022-10-20 02:25:47 +0800

    add partial HomeScreen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unc_ai_surveillance_system_app/app_variables.dart';
-import 'package:unc_ai_surveillance_system_app/models/user.dart';
 import 'package:unc_ai_surveillance_system_app/screens/home_screen.dart';
 
-MaterialPageRoute routeToHome(
-        {required AppVariables appVariables, required User user}) =>
-    MaterialPageRoute(
-        builder: (context) => HomeScreen(vars: appVariables, user: user));
+MaterialPageRoute routeToHome(AppVariables appVariables) => MaterialPageRoute(
+    builder: (context) => HomeScreen(appVariables: appVariables));
