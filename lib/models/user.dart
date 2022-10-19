ce7528adca151bcd52d6986a7c2ccdbc0f03a436
neilcantorne <cantorne.neil@gmail.com>
2022-10-20 02:07:02 +0800

    implement login

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'package:unc_ai_surveillance_system_app/app_variables.dart';
+
 class User {
   String id;
   String username;
@@ -22,4 +24,26 @@ class User {
         "first-name": firstName,
         "last-name": lastName,
       };
+
+  static Future<void> login(
+      {required AppVariables appVariables,
+      required String username,
+      required String password}) async {
+    final response = await appVariables.httpClient.post("/users/login", data: {
+      "username": username,
+      "password": password,
+      "device-os": appVariables.device.os,
+      "device-name": appVariables.device.name,
+      "device-signature": appVariables.device.signature
+    });
+
+    if (response.statusCode == 200) {
+      final jwt = response.data.toString();
+
+      // Save JSON Web Token to cookies
+      if (appVariables.httpClient.options.headers.containsKey("Cookie")) {
+        appVariables.httpClient.options.headers['Cookie'] = "jwt=$jwt";
+      }
+    }
+  }
 }
