13cbdcbc4465f700d72e2526b4a91a8c350f1418
neilcantorne <cantorne.neil@gmail.com>
2022-12-31 16:49:14 +0800

    remove routes

--- lib/routes.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:flutter/material.dart';
-// import 'package:unc_ai_surveillance_system_app/app_vars.dart';
-// import 'package:unc_ai_surveillance_system_app/models/user.dart';
-// import 'package:unc_ai_surveillance_system_app/screens/home_screen.dart';
-//
-// MaterialPageRoute routeToHome({required AppVars vars, required User user}) =>
-//     MaterialPageRoute(builder: (context) => HomeScreen(vars: vars, user: user));
